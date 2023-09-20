1e4caa77c2e77f82a63c80a3c4bf726ed35c2f6e
KamandeRick <rickmwangi237@gmail.com>
2023-09-20 13:51:06 -0700

    add function to print error message if command not found

---
+++ error.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * error_not_found - Prints custommessage if command isnntnot found
+ * @arvs: command-line args
+ * @tokArray: tokenized array of strings
+ * @cmdNum: counter
+ * Return: void
+ */
+int error_not_found(char **arvs, char **tokArray, size_t cmdNum)
+{
+	char *error_str;
+	char *cmdNum_str = intToStr(cmdNum);
+	int size = (strlen(arvs[0]) + (2 * strlen(": ")) +
+			digCounter(cmdNum) + strlen(tokArray[0]) +
+			strlen(": not found\n") + 1);
+
+	charMalloc(&error_str, size, "error_not_found Error: malloc error");
+	strcpy(error_str, arvs[0]);
+	strcat(error_str, ": ");
+	strcat(error_str, cmdNum_str);
+	strcat(error_str, ": ");
+	strcat(error_str, tokArray[0]);
+	strcat(error_str, ": not found\n");
+	strcat(error_str, "\0");
+
+	write(STDERR_FILENO, error_str, strlen(error_str));
+	free(error_str);
+	free(cmdNum_str);
+	return (127);
+}
+
